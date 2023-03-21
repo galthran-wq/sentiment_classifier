f85a7aabe8f2996c610466e2a0cb83c6fc6f790e
unknown <alcaponeforum@gmail.com>
2023-03-21 19:10:11 +0300

    adding inference

--- sent_class/views.py
+++ sent_class/views.py
@@ -1,23 +1,45 @@
+import json
+import torch
+from transformers import DistilBertForSequenceClassification, DistilBertTokenizer
 from .forms import SentenceUploadForm
 
 from django.shortcuts import render
 
+tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
+model = DistilBertForSequenceClassification.from_pretrained("galthran/distilbert-base-uncased-acllm")
+
+
+def get_prediction(text):
+    label_to_rating = {}
+    for i in [0, 2, 3]:
+        label_to_rating[i] = i + 1
+    for i in [4, 5, 6, 7]:
+        label_to_rating[i] = i + 2
+    label_to_rating[1] = 10
+    label_to_rating[0] = 1
+
+    inputs = tokenizer(text, return_tensors="pt")
+    ratings_dist = model(**inputs).logits.softmax(dim=-1)[0]
+    return json.dumps({
+        (label_to_rating[i]): (conf.item())
+        for i, conf in enumerate(ratings_dist)
+    })
+
+
+
 # Create your views here.
 def index(request):
-    image_uri = None
-    predicted_label = None
-
+    predicted_rating = None
     if request.method == 'POST':
         # in case of POST: get the uploaded image from the form and process it
         form = SentenceUploadForm(request.POST, request.FILES)
         if form.is_valid():
             text = form.cleaned_data['sentence']
             print(text)
-
-            # try:
-            #     predicted_label = get_prediction(image_bytes)
-            # except RuntimeError as re:
-            #     print(re)
+            try:
+                predicted_rating = get_prediction(text)
+            except RuntimeError as re:
+                print(re)
 
     else:
         # in case of GET: simply show the empty form for uploading images
@@ -27,7 +49,6 @@ def index(request):
     # pass the form, image URI, and predicted label to the template to be rendered
     context = {
         'form': form,
-        'image_uri': image_uri,
-        'predicted_label': predicted_label,
+        'predicted_rating': predicted_rating,
     }
     return render(request, 'index.html', context)
